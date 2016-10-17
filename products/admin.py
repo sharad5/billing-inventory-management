ab2ed92d28f113755392ca38f5ed0072b31cab10
Sharad <sharaddargan@gmail.com>
2016-10-17 18:35:40 +0530

    Added Basic Admin Panel for product management

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from products.models import *
 # Register your models here.
+class AttributeInline(admin.TabularInline):
+    model=Attribute
+    extra=0
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_filter=['date_created','date_updated']
+    list_display=('id','upc','title','stock')
+    search_fields=('upc','title')
+    inlines=[AttributeInline]
+
+admin.site.register(Product,ProductAdmin)
+admin.site.register(Category)
--- products/models.py
+++ products/models.py
@@ -2,17 +2,18 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
-    name=models.TextField(blank=False,null=False)
+    name=models.CharField(max_length=1000,blank=False,null=False)
     date_created=models.DateField(auto_now_add=True, blank=True)
     date_updated=models.DateField(auto_now=True)
+    class Meta:
+        verbose_name_plural = "Categories"
 
-class Attribute(models.Model):
-    name=models.TextField(blank=False,null=False)
-    value=models.TextField(blank=False,null=False)
+    def __str__(self):
+        return self.name
 
 class Product(models.Model):
-    upc=models.TextField(blank=True, null=False)
-    title=models.TextField(blank=False, null=False)
+    upc=models.CharField(max_length=1000,blank=True, null=False)
+    title=models.CharField(max_length=1000,blank=False, null=False)
     description=models.TextField(blank=True,null=True)
     date_created=models.DateField(auto_now_add=True, blank=True)
     date_updated=models.DateField(auto_now=True)
@@ -20,7 +21,11 @@ class Product(models.Model):
     selling_price=models.FloatField(blank=True,null=True,default=None)
     stock=models.PositiveIntegerField(default=0)
     category=models.ForeignKey(Category)
-    attributes=models.ManyToManyField(Attribute)
 
     def __str__(self):
         return self.title+" ("+self.upc+")"
+
+class Attribute(models.Model):
+    name=models.CharField(max_length=1000,blank=False,null=False)
+    value=models.CharField(max_length=1000,blank=False,null=False)
+    product=models.ForeignKey(Product)
